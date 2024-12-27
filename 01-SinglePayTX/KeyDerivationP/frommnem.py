c29aed714c99650a614df6df11f8e6f789b19d27
Giuseppe Persiano <giuper@gmail.com>
2024-12-27 07:18:04 +0100

    commenting frommnem.py

--- 01-SinglePayTX/KeyDerivationP/frommnem.py
+++ 01-SinglePayTX/KeyDerivationP/frommnem.py
@@ -12,48 +12,56 @@ from algosdk import account, mnemonic
 
 folder="../Accounts/"
 prefix="-kdpy"
+
+print("this script derives the public key in three different ways\n")
+print("\tas second half of the secret key obtained from the mnemonic")
+print("\tby invoking publickey method of the ED25519 library on the secret key")
+print("\tby decoding the address of the account")
+print("keys are written in folder\n",folder)
+print("the script needs the mnemonic that is found in folder",folder,"in a file called <account name>.mnem")
+
 if (len(sys.argv)!=2):
     print("Usage: "+sys.argv[0]+" <account name>")
-    print("keys are written in folder",folder)
     exit()
 accountName=sys.argv[1]
-fileSK64=folder+accountName+prefix+".sk64"
-fileSK=folder+accountName+prefix+".sk"
-fileAddr=folder+accountName+prefix+".addr"
-filePKHex=folder+accountName+prefix+".pkhex"
+    
+##files where intermediate results will be stored
+fileSK64=folder+accountName+prefix+".sk64"               #the base64-encoded secret key
+fileSK=folder+accountName+prefix+".sk"                   #the secret key in binary
+fileAddr=folder+accountName+prefix+".addr"               #the address
+filePKHex=folder+accountName+prefix+".pkhex" 
 filePK=folder+accountName+prefix+".pk"
-filePKSDKed25519=folder+accountName+prefix+"-ed25519.pk"
+filePKSDKed25519=folder+accountName+prefix+"-ed25519.pk" #the public key computed by the python implementation of ED25519
 
 print("Derive secret and public key from mnem\n");
 with open(accountName+".mnem",'r') as f:
     Mnem=f.read()
 
-SK64=mnemonic.to_private_key(Mnem)
-Addr=account.address_from_private_key(SK64)
+SK64=mnemonic.to_private_key(Mnem)                     #obtain base64-encoded secret key from mnemonic
 print(f'{"SK64 from mnem:":35s}{SK64:s}')
 print(f'{"written in file:":35s}{fileSK64:s}\n')
 with open(fileSK64,"w") as f:
     f.write(SK64)
 
-SK=base64.b64decode(SK64)
+SK=base64.b64decode(SK64)                               #obtain the secret key by based64 decoding SK64
 print(f'{"SK from decoding SK64:":35s}{SK.hex():s}')
 print(f'{"written in file:":35s}{fileSK:s}\n')
 with open(fileSK,"wb") as f:
     f.write(SK)
-print(f'{"private key from splitting SK:":35s}{SK[0:32].hex()}')
-print(f'{"public key from splitting SK:":35s}{SK[32:].hex()}\n')
+print(f'{"private key from splitting SK:":35s}{SK[0:32].hex()}')  #first half is the actual secret key
+print(f'{"public key from splitting SK:":35s}{SK[32:].hex()}\n')  #second half is the public key
 
-pk25519=publickey(SK[0:32])
+pk25519=publickey(SK[0:32])                                       #obtain the public key through the ED25519 python module
 print(f'{"PK from SK through ed25519new:":35s}{pk25519.hex():s}')
 print(f'{"written in file:":35s}{filePKSDKed25519:s}\n')
 with open(filePKSDKed25519,"wb") as f:
     f.write(pk25519)
 
-Addr=account.address_from_private_key(SK64)
+Addr=account.address_from_private_key(SK64)            #obtain address from base64-encoded secret key
 print(f'{"Algorand address from SK64:":35s}{Addr:s}')
 print(f'{"written in file:":35s}{fileAddr:s}')
 with open(fileAddr,"w") as f:
     f.write(Addr)
 
-AddrDec=base64.b32decode(Addr+'======')
-print(f'{"Public key from Addr ":35s}{AddrDec[:-4].hex()}')
+AddrDec=base64.b32decode(Addr+'======')                #add padding as required by base32 encoding and then decode
+print(f'{"Public key from Addr ":35s}{AddrDec[:-4].hex()}') #this will give the public key again
