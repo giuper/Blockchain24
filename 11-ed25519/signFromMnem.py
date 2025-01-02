680d9d32bcabdb4f4e04ea4da9b77a68b8ac9215
Giuseppe Persiano <giuper@gmail.com>
2025-01-02 20:44:24 +0100

    progAddr on the command line

--- 11-ed25519/signFromMnem.py
+++ 11-ed25519/signFromMnem.py
@@ -8,13 +8,18 @@ from algosdk import mnemonic
 from Cryptodome.Hash import SHA512
 
 
-if (len(sys.argv)!=4):
-    print("Usage: "+sys.argv[0]+" <account name> <tok file> <data to be signed>")
+if (len(sys.argv)<4):
+    print("Usage: "+sys.argv[0]+" <account name> <tok file> <data to be signed> [address of program]")
     exit()
 
 MnemFile=sys.argv[1]+".mnem"
 TokFile=sys.argv[2]+".tok"
 DataToBeSigned=sys.argv[3]
+if len(sys.argv)==5:
+    progAddr=sys.argv[4]
+else:
+    progAddr=None
+    
 
 with open(MnemFile,'r') as f:
         Mnem=f.read()
@@ -32,16 +37,16 @@ with open(MnemFile+".sk",'wb') as f:
     f.write(SK[0:32])
 
 print()
-progAddr="SXWZZSNJZIKBWG2QZHVVAMEFGFJIKRYXRWYN3R7S54GGJ3CGVFZB7FTEBI"
 print(f'{"computing hash of program:":30s}')
-print(f'{"starting from address:":30s}{progAddr:s}')
-if len(progAddr)%8!=0:    #padding with right number of =
-    progAddr=progAddr+"="*(8-len(progAddr)%8)
-print(f'{"padded address:":30s}{progAddr:s}')
-final=base64.b32decode(progAddr)
-print(f'{"length in bytes":30s}{len(final):d}')
-print(f'{"final1":28s}{final}')
-print()
+if progAddr is not None:
+    print(f'{"starting from address:":30s}{progAddr:s}')
+    if len(progAddr)%8!=0:    #padding with right number of =
+        progAddr=progAddr+"="*(8-len(progAddr)%8)
+    print(f'{"padded address:":30s}{progAddr:s}')
+    final=base64.b32decode(progAddr)
+    print(f'{"length in bytes":30s}{len(final):d}')
+    print(f'{"final1":28s}{final}')
+    print()
 
 print(f'{"computing hash of program:":30s}')
 print(f'{"starting from program:":30s}{TokFile:s}')
@@ -59,15 +64,16 @@ checksum=hh.digest()[-4:]
 final2=ProgPrefixH+checksum
 print(f'{"length in bytes of hash":30s}{len(final2):d}')
 print(f'{"final2":28s}{final2}')
-if final2==final:
-    print("final1==final2")
-else:
-    print("final1!=final2")
-    exit()
+if progAddr is not None:
+    if final2==final:
+        print("final1==final2")
+    else:
+        print("final1!=final2")
+        exit()
 
 print()
 
-unMarshaled=final[:-4]
+unMarshaled=final2[:-4]
 print(f'{"unmarshaled":28s}{unMarshaled}')  #see go-algorand/cmd/goal/tealsign.go:145
 m=b'ProgData'+unMarshaled+bytes(DataToBeSigned,'utf-8') #domain separation
 print(f'{"message to be signed:":28s}{m}')
