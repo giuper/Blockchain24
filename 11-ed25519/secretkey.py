0dbddaafd3c90aea89bbca16cc8bf9422f400914
Giuseppe Persiano <giuper@gmail.com>
2025-04-07 18:08:15 +0200

    fixing bug

--- 11-ed25519/secretkey.py
+++ 11-ed25519/secretkey.py
@@ -8,25 +8,28 @@ if (len(sys.argv)!=2):
     print("Usage: "+sys.argv[0]+" <account name>")
     exit()
 
-account=sys.argv[1]
+acct=sys.argv[1]
 
-mnemF=account+".mnem"
+mnemF=acct+".mnem"
 with open(mnemF,'r') as f:
     Mnem=f.read()
 
 SK64=mnemonic.to_private_key(Mnem)
-print(creatorSK64)
-with open(account+".sk64","w") as f:
+print(f'{"The secret key from mnem":28s}{SK64}')
+print(f'{"Found in file":28s}{acct+".sk64"}')
+with open(acct+".sk64","w") as f:
     f.write(SK64)
 
-SK=base64.b64decode(creatorSK64)
-##print(creatorSK)  
-with open(account+".skk","wb") as f:
+SK=base64.b64decode(SK64)
+with open(acct+".skk","wb") as f:
     f.write(SK)
+print(f'{"Binary key found in file":28s}{acct+".skk"}')
 
 Addr=account.address_from_private_key(SK64)
-print(Addr)
-with open(account+".addr","w") as f:
+print(f'{"The address from the secret key":28s}{Addr}')
+print(f'{"Found in file":28s}{acct+".addr"}')
+with open(acct+".addr","w") as f:
     f.write(Addr)
 
 ##same effect can be obtained with algokey import
+##but without having to install a node
