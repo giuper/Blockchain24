e1d8b16db8999e3c60e38de6a85c45dc2cd98785
Giuseppe Persiano <giuper@gmail.com>
2025-04-09 13:17:04 +0200

    factoring out function

--- 11-ed25519/publickey.py
+++ 11-ed25519/publickey.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3.10
 from ed25519 import publickey_unsafe as publickey
-from ed25519 import decodepoint, encodepoint
+from ed25519 import decodepoint, encodepoint, edwards_double
 import base64
 import sys
 from algosdk import mnemonic
 
 
-if (len(sys.argv)<2):
-    print("Usage: "+sys.argv[0]+" <account name>")
-    exit()
+def mnemonicToPublic(mnemFile):
+    with open(mnemFile,'r') as f:
+            mnem=f.read()
+    SK64=mnemonic.to_private_key(mnem)
+    SK=base64.b64decode(SK64) 
+    return SK,SK[32:]
 
-MnemFile=sys.argv[1]+".mnem"
-with open(MnemFile,'r') as f:
-        Mnem=f.read()
-SK64=mnemonic.to_private_key(Mnem)
-print(f'{"b64 secret key from mnem":30s}{SK64}')
-SK=base64.b64decode(SK64) #see https://developer.algorand.org/docs/get-details/accounts/
-print(f'{"extended SK":30s}{SK.hex()}')
+if __name__=='__main__':
+    if (len(sys.argv)<2):
+        print("Usage: "+sys.argv[0]+" <account name>")
+        exit()
 
-PK=publickey(SK[0:32])
-print(f'{"The public key":30s}')
-print(f'{"from publickey method:":30s}{PK.hex()}')
-print(f'{"from splitting extended SK:":30s}{SK[32:].hex()}')
+    mnemFile=sys.argv[1]+".mnem"
+    SK,PK=mnemonicToPublic(mnemFile)
+    print(f'{"extended SK":30s}{SK.hex()}')
+
+    PKm=publickey(SK[0:32])
+    print(f'{"The public key":30s}')
+    print(f'{"from publickey method:":30s}{PKm.hex()}')
+    print(f'{"from splitting extended SK:":30s}{PK.hex()}')
 
